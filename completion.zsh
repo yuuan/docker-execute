0b9c1b9d32e6b0e49545e4340f2e7e107dd4f52d
yuuAn <nora@yuuan.net>
2021-02-25 21:12:46 +0900

    Zsh で補完するための設定を用意

---
+++ completion.zsh
@@ -0,0 +1,7 @@
+compdef '_dispatch composer composer' composer-exec
+compdef '_dispatch artisan artisan' artisan-exec
+compdef '_dispatch yarn yarn' yarn-exec
+compdef '_dispatch bundle bundle' bundle-exec
+compdef '_dispatch rails rails' rails-exec
+compdef '_dispatch rspec rspec' rspec-exec
+compdef '_dispatch mysql mysql' mysql-exec
